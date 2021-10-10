1ebcb9b32138e0834b387795fcd5a850ccda7404
ktnk-dev <63897347+ktnk-dev@users.noreply.github.com>
2021-10-10 11:30:50 +0300

    код

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests, bs4
+url = 'https://anekdot.ru/random/anekdot'                        
+h   = {"User-Agent":"1"}                   # сайт не пускает без header
+web = requests.get(url, headers=h).text    # Получение кода веб-сайта, где расположены случайные анекдоты
+
+bs      = bs4.BeautifulSoup(web, "lxml")                             
+text    = str(bs.find_all(class_="topicbox")[1].find(class_="text"))  # получаем элемент, в котором написан текст анекдота
+anekdot = text[18:-6].replace("<br/>","\n")                           # удаляем лишние теги, которые попали в наш текст. заменяем тег переноса на \n
+
+print(f"\n{anekdot}\n")
