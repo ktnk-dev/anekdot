7d306bfada8ac83b2e7d4cc4030c2daafc4f1fcf
ktnk ♡ <63897347+ktnk-dev@users.noreply.github.com>
2022-04-24 23:52:27 +0300

    теперь даже обновленный сайт не помешает работе
    а еще можно спокойно импортировать и писать anekdot.get()
    выглядит прикольно

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import requests, bs4
-url = 'https://anekdot.ru/random/anekdot'                        
-h   = {"User-Agent":"1"}                   # сайт не пускает без header
-web = requests.get(url, headers=h).text    # Получение кода веб-сайта, где расположены случайные анекдоты
+def get():
+    url = 'https://anekdot.ru/random/anekdot'                        
+    h   = {"User-Agent":"1"}                   # сайт не пускает без header
+    web = requests.get(url, headers=h).text    # Получение кода веб-сайта, где расположены случайные анекдоты
 
-bs      = bs4.BeautifulSoup(web, "lxml")                             
-text    = str(bs.find_all(class_="topicbox")[1].find(class_="text"))  # получаем элемент, в котором написан текст анекдота
-anekdot = text[26:-6].replace("<br/>","\n")                           # удаляем лишние теги, которые попали в наш текст. заменяем тег переноса на \n
+    bs      = bs4.BeautifulSoup(web, "lxml")                             
+    result    = str(bs.find_all(class_="topicbox")[1].find(class_="text"))  # получаем элемент, в котором написан текст анекдота
+    text = result.replace("<br/>","\n")                           # удаляем лишние теги, которые попали в наш текст. заменяем тег переноса на \n
+    text = text.split(">")
+    text[0] = ""
+    text = ''.join(text)
+    text = text.split("<")
+    text[-1] = ""
+    text = ''.join(text)
+    return text
 
-print(f"\n{anekdot}\n")
